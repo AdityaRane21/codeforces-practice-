3f4d9cf2c39d0998a71c6ceb1495113b797409e4
mavrix21 <150045973+AdityaRane21@users.noreply.github.com>
2025-12-08 22:24:16 +0530

    Add solution for counting adjacent stones
    Implement solution to count adjacent stones.

---
+++ 800 /266A Stones on the Table.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, min_count = 0;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    for (int i = 1; i < n; i++) {
+        if (s[i] == s[i - 1])
+            min_count++;
+    }
+
+    cout << min_count;
+    return 0;
+}
