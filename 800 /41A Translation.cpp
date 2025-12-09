d17ee5142c2ccfc5cdd43c23928dcab6f7f8a021
mavrix21 <150045973+AdityaRane21@users.noreply.github.com>
2025-12-09 21:52:22 +0530

    Implement string reversal comparison

---
+++ 800 /41A Translation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    reverse(a.begin(), a.end());
+
+    cout << (a == b ? "YES" : "NO");
+
+    return 0;
+}
