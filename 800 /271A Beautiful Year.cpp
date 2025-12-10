97458341e11ff244e18699dbebee4bc1ccfe92bf
mavrix21 <150045973+AdityaRane21@users.noreply.github.com>
2025-12-10 23:05:05 +0530

    Add solution for '271A Beautiful Year' problem

---
+++ 800 /271A Beautiful Year.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n; cin>>n;
+	n=n+1;
+	while(true){
+    	int o = n%10;
+    	int te = n/10%10;
+    	int h = n/100%10;
+    	int th = n/1000%10;
+    	if(o!=te && o!=h && o!=th && te!=h && te!=th && h!=th) {
+    	    cout<<n;
+    	    return 0;
+	    } 
+	    else n++;
+	}
+}
